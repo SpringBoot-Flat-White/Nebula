7e4661642646f62f0886213466b3f1c7bd382ec3
Johan192004 <johanruma@gmail.com>
2025-11-05 19:06:23 -0500

    feat: initialize Spring Boot project with Maven wrapper and essential configurations

---
+++ src/main/java/com/nebula/nebulaCloud/NebulaCloudApplication.java
@@ -0,0 +1,13 @@
+package com.nebula.nebulaCloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NebulaCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NebulaCloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nebula/nebulaCloud/NebulaCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nebula.nebulaCloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NebulaCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
