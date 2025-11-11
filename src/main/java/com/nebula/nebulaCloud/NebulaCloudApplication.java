b80881781bd9c356c99ea75d017d2f0b65f5ea11
isabellaPulgarin <isabellapul7@gmail.com>
2025-11-10 21:58:57 -0500

    feat: add the config properties

--- src/main/java/com/nebula/nebulaCloud/NebulaCloudApplication.java
+++ src/main/java/com/nebula/nebulaCloud/NebulaCloudApplication.java
@@ -1,9 +1,12 @@
 package com.nebula.nebulaCloud;
 
+import com.nebula.nebulaCloud.config.EngineConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(EngineConfig.class)
 public class NebulaCloudApplication {
 
 	public static void main(String[] args) {
