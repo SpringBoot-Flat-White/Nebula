6bb0e359d27b240b3968f15c1a5c457f1428e638
isabellaPulgarin <isabellapul7@gmail.com>
2025-11-13 14:23:05 -0500

    feat: add the planRepository

---
+++ src/main/java/com/nebula/nebulaCloud/repository/PlanRepository.java
@@ -0,0 +1,7 @@
+package com.nebula.nebulaCloud.repository;
+
+import com.nebula.nebulaCloud.model.Plan;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlanRepository extends JpaRepository<Plan,Long> {
+}
